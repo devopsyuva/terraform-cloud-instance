491b88ece14ad33a544c964d73ce7ed26badd54d
Sudheer Reddy Duba <sudheer.reddy.duba@gmail.com>
2021-11-03 03:20:03 +0000

    Updated instance type to t3.micro

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami                         = "ami-09e67e426f25ce0d7" #Ubuntu 20.04 LTS Free Tier Image
-  instance_type               = "t2.micro"
+  instance_type               = "t3.micro"
   key_name                    = "sudhams_virginia_demo"
   subnet_id                   = aws_subnet.public_subnet.id
   vpc_security_group_ids      = [aws_security_group.allow_http_ssh.id]
